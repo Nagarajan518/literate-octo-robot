b9601fcfd44789167319359d7283f1da4acabec0
Nagarajan518 <109498238+Nagarajan518@users.noreply.github.com>
2022-07-18 10:13:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('":heart:"BreakFast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinanch & Rocket Snoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avocado Toast')
+streamlit.header('BreakFast Menu')
+streamlit.text(':meal: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':spinach: Kale, Spinanch & Rocket Snoothie')
+streamlit.text(':hen: Hard-Boiled Free-Range Egg')
+streamlit.text(':avocado: Avocado Toast')
