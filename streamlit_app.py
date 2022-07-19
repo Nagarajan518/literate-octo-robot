f9c57952d70b2ab1d9efc1fb2341d65cd3c0b834
Nagarajan518 <109498238+Nagarajan518@users.noreply.github.com>
2022-07-19 12:16:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,8 @@ streamlit.dataframe(fruityvice_normalized)
 #
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.fetchall()
+my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.header("Fruit load list contans:")
 streamlit.dataframe(my_data_row)
